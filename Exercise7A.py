4c313d04435b647bc53911b90d38af9877300095
Prabish Khadka Chhetri <62901562+prabishkhadka@users.noreply.github.com>
2023-03-07 17:24:26 -0600

    Update Exercise7A.py

--- Exercise7A.py
+++ Exercise7A.py
@@ -15,7 +15,8 @@ park = fcList[1]
 arcpy.MakeFeatureLayer_management(park, "park_2")
 
 # Open UpdateCursor to interate through the features in citBndry feature class. SHAPE@- to get the boundry of each feature. and "HASParkAndRide" to update.
-
+#initialize variable
+total1 = 0
 try:
     with arcpy.da.UpdateCursor(citBndry, ["SHAPE@", "HasParkAndRide"]) as cursor:
         for row in cursor:
@@ -27,11 +28,20 @@ try:
             count = int(arcpy.management.GetCount("park_2").getOutput(0))
             #Update True to the field "HasParkAndRide" = True if geometry has park feature.
             if count > 0:
+                #Get the number of cities with atleast one park
+                total1 += 1
                 row[1] = "True"
                 cursor.updateRow(row)
     #Clean up cursor and the temporary feature class
     del cursor
     arcpy.management.Delete("park_2")
+    #Get total number of cities in the give city boundary file
+    total = int(arcpy.GetCount_management(fcList[0]).getOutput(0))
+    #Calculate percentage of cities with atleast one park facilities
+    percentage = (total1/total)*100
+    print(f"The percentage of cities with atleast one park and ride facilities is : {percentage}")
+
+
 except:
     print(arcpy.GetMessage())
 
